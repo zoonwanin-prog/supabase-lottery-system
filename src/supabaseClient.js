6adc476a4cbc8631e467d8231c9e71ac3eb14dda
zoonwanin-prog <zoonwanin@gmail.com>
2026-04-08 07:46:50 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-client'
 
-const supabaseUrl = 'https://supabase.com/dashboard/project/xtfrfedwxzltvsebttkb'
+const supabaseUrl = 'https://xtfrfedwxzltvsebttkb.supabase.co'
 const supabaseAnonKey = 'sb_publishable_FPN9E_UaJ3S6yYVff4bAyw_V7ZnTj0I'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
