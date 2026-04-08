0ad02a511763889b977e5e68490645943447382c
zoonwanin-prog <zoonwanin@gmail.com>
2026-04-08 07:57:54 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,4 @@
-import { createClient } from '@supabase/supabase-client'
-
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://xtfrfedwxzltvsebttkb.supabase.co'
 const supabaseAnonKey = 'sb_publishable_FPN9E_UaJ3S6yYVff4bAyw_V7ZnTj0I'
 
